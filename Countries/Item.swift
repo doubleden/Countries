bdbb41ea80b3179605605ca9156004ea463e800b
Denis Denisov <denis.denisov8701@gmail.com>
2024-12-24 16:13:12 +0100

    Initial Commit

---
+++ Countries/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Countries
+//
+//  Created by Denis Denisov on 24/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
